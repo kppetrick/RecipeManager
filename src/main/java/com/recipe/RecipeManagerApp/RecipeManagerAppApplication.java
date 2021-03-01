32cdcca4a8657c5e85290fdb0b30aaa1235066d7
kppetrick <kppetrick@gmail.com>
2021-03-01 16:02:02 -0500

    first real commit

---
+++ src/main/java/com/recipe/RecipeManagerApp/RecipeManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.RecipeManagerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipe/RecipeManagerApp/RecipeManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.RecipeManagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
