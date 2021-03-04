93b1401c693998c907f5e4740bee0b556029d009
Gunjan Pandya <gunjanpandya@santoshs-air.lan>
2021-03-04 14:14:52 -0500

    depndency for H2 and hello world end point

--- src/main/java/RecipeAppRunner/Controllers/RecipePostController.java
+++ src/main/java/RecipeAppRunner/Controllers/RecipePostController.java
@@ -1,9 +1,19 @@
 package RecipeAppRunner.Controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j // for logging
 public class RecipePostController {
+
+    @GetMapping
+    public ResponseEntity<?> HelloWorld()
+    {
+        return new ResponseEntity<>("Hello World" , HttpStatus.OK);
+    }
 }
