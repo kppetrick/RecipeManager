ca6f49b84a82889f616959ba5a3cbf05d4b539e5
kppetrick <kppetrick@gmail.com>
2021-03-04 13:18:05 -0500

    nothing

--- src/main/java/RecipeAppRunner/Entities/RecipePost.java
+++ src/main/java/RecipeAppRunner/Entities/RecipePost.java
@@ -1,7 +1,34 @@
 package RecipeAppRunner.Entities;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+import javax.persistence.*;
+
+import java.util.Set;
+
+import static javax.persistence.CascadeType.ALL;
 
 @Entity
 public class RecipePost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String Name;
+
+
+    @OneToMany(mappedBy = "ingredients", cascade = ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @OrderBy
+    @JsonBackReference
+    private Set<Ingredients> ingredients;
+
+    public RecipePost() {
+    }
+
+    public RecipePost(Long id, String name, Set<Ingredients> ingredients) {
+        this.id = id;
+        Name = name;
+        this.ingredients = ingredients;
+    }
 }
