12c00d76de8c6c3d201d3d862188868bd3b3f52c
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-03-06 16:10:53 -0500

    Added DeleteMapping(RecipeController) and delete logic(RecipeService)

--- src/main/java/RecipeAppRunner/Controllers/RecipePostController.java
+++ src/main/java/RecipeAppRunner/Controllers/RecipePostController.java
@@ -1,19 +1,32 @@
 package RecipeAppRunner.Controllers;
 
+import RecipeAppRunner.Services.RecipePostServices;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j // for logging
 public class RecipePostController {
 
+    private final RecipePostServices recipePostServices;
+
+    public RecipePostController(RecipePostServices recipePostServices) {
+        this.recipePostServices = recipePostServices;
+    }
+
     @GetMapping
     public ResponseEntity<?> HelloWorld()
     {
         return new ResponseEntity<>("Hello World" , HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteRecipe(@PathVariable("id") Long id){
+        recipePostServices.deleteRecipe(id);
     }
 }
--- src/main/java/RecipeAppRunner/Services/RecipePostServices.java
+++ src/main/java/RecipeAppRunner/Services/RecipePostServices.java
@@ -1,7 +1,26 @@
 package RecipeAppRunner.Services;
 
+import RecipeAppRunner.Repositories.RecipePostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecipePostServices{
+
+    private final RecipePostRepo recipePostRepo;
+
+    @Autowired
+    public RecipePostServices(RecipePostRepo recipePostRepo) {
+        this.recipePostRepo = recipePostRepo;
+    }
+
+    public void deleteRecipe(Long id) {
+        boolean recipeExists = recipePostRepo.existsById(id);
+        if (!recipeExists){
+            throw new IllegalStateException(
+                    "Recipe with id " + id + " does not exist."
+            );
+        }
+        recipePostRepo.deleteById(id);
+    }
 }
