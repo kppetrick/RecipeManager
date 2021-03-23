9606b96a7cf0e06bdc8ae4159029431d58fba623
christianpirhalla <chrispir@udel.edu>
2021-03-22 21:38:26 -0400

    progress

--- src/main/java/RecipeAppRunner/Services/RatingService.java
+++ src/main/java/RecipeAppRunner/Services/RatingService.java
@@ -7,6 +7,8 @@ import RecipeAppRunner.Repositories.RecipePostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RatingService {
     @Autowired
@@ -15,11 +17,22 @@ public class RatingService {
     @Autowired
     RecipePostRepo recipePostRepo;
 
+    @Autowired
+    RecipePostServices recipePostServices;
+
     public void rate(Rating rating) {
         RecipePost recipePost = recipePostRepo.findById(rating.getRecipePost().getId()).orElseThrow(() -> new RuntimeException("Post Not Found"));
         // still need to implement logic for not allowing double ratings
 
+
         ratingRepo.save(rating);
+        recipePostServices.updateRating(recipePost.getId(), recipePostServices.calculateRating(recipePost.getId()));
+
         recipePostRepo.save(recipePost);
     }
+
+    public List<Rating> getAllRatingsForRecipePost(RecipePost recipePost){
+        return ratingRepo.findAllByRecipePost(recipePost);
+    }
+
 }
